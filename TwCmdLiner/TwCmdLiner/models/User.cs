711101bec770c82916b02657fa1cf9d1089f22bd
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-07-10 22:48:51 -0600

    add base config user

---
+++ TwCmdLiner/TwCmdLiner/models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwCmdLiner
+{
+    class User
+    {
+        private string _nickname;
+        private string _password;
+
+        public User(string vNickname, string vPassword) {
+            _nickname = vNickname;
+            _password = vPassword;
+        }
+
+        public string GetNickname() {
+            return _nickname;
+        }
+
+        public void SetNickname(string vNickName) {
+            _nickname = vNickName;
+        }
+
+        public string GetPassword() {
+            return _password;
+        }
+
+        public void SetPassword(string vPassword) {
+            _password = vPassword;
+        }
+    }
+}
