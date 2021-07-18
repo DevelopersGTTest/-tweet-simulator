ba51779d5cf22a93b90b3419dc65ad9829333162
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-07-17 23:47:39 -0600

    add enum validator

--- TwCmdLiner/TwCmdLiner/CmdMenu/Prompt.cs
+++ TwCmdLiner/TwCmdLiner/CmdMenu/Prompt.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwCmdLiner.Enums;
 
 namespace TwCmdLiner.CmdMenu
 {
     class Prompt
     {
+        int opt = 0;
+
+        enum Types {
+            USERS = 1,
+            TWEET = 2
+        }
+
         public void ReceivedData(User user) {
             Console.WriteLine("hello there..." + user.nickname );
             Console.WriteLine("Select your operation");
-            Console.WriteLine("1. Users");
-            Console.WriteLine("1. Tweets");
+            Console.WriteLine("2. Users");
+            Console.WriteLine("3. Tweets");
+
+            opt = Int16.Parse(Console.ReadLine());
+
+            if (Enum.IsDefined(typeof(Types), opt)) {
+                Console.WriteLine("IS VALID.....");
+            } else {
+                Console.WriteLine("not finded");
+            }
+
+            Console.WriteLine("OPT SELECTED IS " + opt);
         }
 
     }
