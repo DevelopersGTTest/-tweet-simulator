84103b6c34be40b1f6254841673395bca2a54192
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-10-20 16:14:31 -0600

    fix scripts

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -13,18 +13,9 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+        public ActionResult Access(string username, string password) {
+            return Content("1");
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
