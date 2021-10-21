563b8defb727660076d692b0e4fadfb40c8110f3
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-10-21 12:44:27 -0600

    add login validations and fix error parse

--- WebApp/WebApp/Controllers/PanelController.cs
+++ WebApp/WebApp/Controllers/PanelController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -12,6 +13,13 @@ namespace WebApp.Controllers
         // GET: Panel
         public ActionResult Index()
         {
+            using (PARROQUIAEntities db = new PARROQUIAEntities()) {
+
+                USUARIO userSession = (USUARIO) Session["User"];
+                if (userSession != null) {
+                    ViewBag.Message = "Bienvenido: " + Convert.ToString(userSession.username);
+                }
+            }
             return View();
         }
     }
