a2012e697576841b5e4c8d5bf6f1695e95caca07
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-10-20 18:32:55 -0600

    refactor login validations

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string message = "")
         {
+            ViewBag.Message = message;
             return View();
         }
 
-        public ActionResult Access(string username, string password) {
-            return Content("1");
+        public ActionResult Login(string username, string password) {
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                using (PARROQUIAEntities db = new PARROQUIAEntities())
+                {
+
+                    USUARIO userDb = db.USUARIOs
+                        .FirstOrDefault(u => u.username == username && u.password == password);
+
+                    if (userDb != null)
+                    {
+                        Session["User"] = userDb;
+                        return RedirectToAction("Index", "Panel");
+                    } else
+                    {
+                        return Index("Usuario / Password Incorrectos");
+                    }
+                }
+
+            } else {
+                return Index("Completa los campos para acceder");
+            }
         }
 
     }
