fc2d8d8f2d05b94a449f9829b3d154a88c8c1365
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-10-21 16:49:51 -0600

    fix ui validations

--- WebApp/WebApp/Controllers/PanelController.cs
+++ WebApp/WebApp/Controllers/PanelController.cs
@@ -10,14 +10,20 @@ namespace WebApp.Controllers
    
     public class PanelController : Controller
     {
+        
+
         // GET: Panel
         public ActionResult Index()
         {
+            ViewBag.IsAdmin = false;
             using (PARROQUIAEntities db = new PARROQUIAEntities()) {
 
                 USUARIO userSession = (USUARIO) Session["User"];
                 if (userSession != null) {
                     ViewBag.Message = "Bienvenido: " + Convert.ToString(userSession.username);
+                    if (userSession.id_rol == 1) {
+                        ViewBag.IsAdmin = true;
+                    }
                 }
             }
             return View();
