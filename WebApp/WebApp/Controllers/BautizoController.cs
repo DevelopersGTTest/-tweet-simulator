fa621cba7bf522926a5cdb7d08518595605fb9e3
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-10-22 22:57:36 -0600

    fix validations

---
+++ WebApp/WebApp/Controllers/BautizoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+using WebApp.Models.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class BautizoController : Controller
+    {
+        // GET: Bautizo
+        public ActionResult Index()
+        {
+            List<ListBautizoViewModel> bautizosList;
+            using (PARROQUIAEntities db = new PARROQUIAEntities())
+            {
+                USUARIO userSession = (USUARIO)Session["User"];
+                if (userSession != null)
+                {
+                    Session["RolId"] = userSession.id_rol;
+                    ViewBag.Message = "Bienvenido: " + Convert.ToString(userSession.username);
+                }
+
+                bautizosList = (from b in db.BAUTIZOes
+                                select new ListBautizoViewModel
+                                {
+                                    id_bautizo = b.id_bautizo,
+                                    id_usuario_registro = b.id_usuario_registro,
+                                    nombre_bautizado = b.nombre_bautizado,
+                                    fecha_bautizo = b.fecha_bautizo,
+                                    nombre_padre = b.nombre_padre,
+                                    nombre_madre = b.nombre_madre,
+                                    nombres_padrinos = b.nombres_padrinos,
+                                    fecha_nacimiento_bautizado = b.fecha_nacimiento_bautizado
+                                }).ToList();
+
+
+            }
+                return View(bautizosList);
+        }
+
+        public ActionResult BackPanel()
+        {
+            return RedirectToAction("Index", "Panel");
+        }
+
+    }
+}
