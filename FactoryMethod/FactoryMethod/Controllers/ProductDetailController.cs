5f2c9148fc3a3c7529d990d5fd1f6a519ba97f41
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-01-09 01:13:07 -0600

    implements factory into view

---
+++ FactoryMethod/FactoryMethod/Controllers/ProductDetailController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace FactoryMethod.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            //factories
+            Earn.LocalEarnFactory localEarnFactory = new Earn.LocalEarnFactory(0.20m);
+
+            // products
+            var localEarn = localEarnFactory.GetEarn();
+
+            // total
+            ViewBag.totalLocal = (total + localEarn.Earn(total));
+
+
+            return View();
+        }
+    }
+}
