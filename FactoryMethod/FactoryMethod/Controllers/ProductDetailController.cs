16a42eb7ccd1e782af17e43d0cfae23b176b024b
p-jacobo2012240 <pablojacobo1998@gmail.com>
2022-01-09 01:54:59 -0600

    add foreign factory and same implementation

--- FactoryMethod/FactoryMethod/Controllers/ProductDetailController.cs
+++ FactoryMethod/FactoryMethod/Controllers/ProductDetailController.cs
@@ -9,13 +9,17 @@ namespace FactoryMethod.Controllers
         {
             //factories
             Earn.LocalEarnFactory localEarnFactory = new Earn.LocalEarnFactory(0.20m);
+            Earn.ForeignEarnFactory foreignEarnFactory = new Earn.ForeignEarnFactory(0.30m, 15);
+
+
 
             // products
             var localEarn = localEarnFactory.GetEarn();
+            var foreignEarn = foreignEarnFactory.GetEarn();
 
             // total
             ViewBag.totalLocal = (total + localEarn.Earn(total));
-
+            ViewBag.totalForeign = total + (foreignEarn.Earn(total));
 
             return View();
         }
